d3d00a25d10589b9e05d258929da59526fc75b17
photran25633@gmail.com <photran25633@gmail.com>
2022-12-12 10:00:25 +0700

    cors

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const io = socket(server,{
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chatloveu.netlify.app/",
         credentials: true,
     },
 });
